f43e1cc7569bf00a937954c409d893c831f0ed62
Felix Nagel <felixnagel105@gmail.com>
2020-07-25 11:58:34 +0200

    projects section updated

--- src/Projects.js
+++ src/Projects.js
@@ -1,18 +1,44 @@
 import React from "react";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Thumbnail from './Thumbnail';
 import './Projects.css';
 import wyder from './images/wyder.png';
+import cocktail from './images/cocktail.png';
+import chat from './images/chat.png'
 
 
 function Projects(props) {
   return (
     <div className="Projects">
       <h1 className="Header">PROJECTS</h1>
-      <Thumbnail
-       link="http://www.wyder.xyz/"
-       image={wyder}
-       title="University Video Platform"
-       category="Website"      />
+      <Row>
+        <Col xs={12} md={6} xl={4} className="mb-3">
+          <Thumbnail
+           link="http://www.wyder.xyz/"
+           image={wyder}
+           title="University Video Platform"
+           category="Website"
+           language="Ruby on Rails"      />
+        </Col>
+        <Col xs={12} md={6} xl={4} className="mb-3">
+           <Thumbnail
+           link="https://damp-refuge-59220.herokuapp.com/"
+           image={cocktail}
+           title="Cocktail Recipe App"
+           category="Website"
+           language="Ruby on Rails"      />
+        </Col>
+        <Col xs={12} md={6} xl={4} className="mb-3">
+           <Thumbnail
+           link="https://fenagel.github.io/chat-redux/"
+           image={chat}
+           title="React Chat"
+           category="Chat Implementation"
+           language="React, Redux"      />
+        </Col>
+      </Row>
+
     </div>
   )
 }
--- src/Thumbnail.js
+++ src/Thumbnail.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
 import './Projects.css';
 
 class Thumbnail extends Component {
@@ -26,20 +26,22 @@ class Thumbnail extends Component {
   }
 
   render() {
-    let projectClass = this.state.isHovered && "Project-Hovered";
     return (
-        <Col xs={12} md={6} className="Project" onMouseLeave={this.handleLeave} onMouseEnter={this.handleEnter}>
+        <Col className="Project" onMouseLeave={this.handleLeave} onMouseEnter={this.handleEnter}>
           <Row className= "Dot-Wrapper">
-            <div id="Dot-1" class="Browser-Dot"></div>
-            <div id="Dot-2" class="Browser-Dot"></div>
-            <div id="Dot-3" class="Browser-Dot"></div>
+            <div id="Dot-1" className="Browser-Dot"></div>
+            <div id="Dot-2" className="Browser-Dot"></div>
+            <div id="Dot-3" className="Browser-Dot"></div>
           </Row>
           {this.state.isHovered ? (
-            <Col className="justify-content-center mt-5">
-              <p>{this.props.title}</p>
-              <p>Category: {this.props.category}</p>
-              <Button className="color" target="blank" href={this.props.link} variant="outline-primary">More Details</Button>
-            </Col>
+            <Row className="justify-content-center mt-3">
+              <Col>
+                <p>{this.props.title}</p>
+                <p>Category: {this.props.category}</p>
+                <p>Built with: {this.props.language}</p>
+                <Button className="Button-Color" target="blank" href={this.props.link} variant="outline-primary">More Details</Button>
+              </Col>
+            </Row>
           ) : (
             <div className="Project-Image">
               <img src={this.props.image} alt="Project"/>
