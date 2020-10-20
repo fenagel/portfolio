6c61811d36739b88c2866f8ad37f9155a5935d5a
Felix Nagel <felixnagel105@gmail.com>
2020-10-20 11:34:03 +0200

    still trying to fix bootstrap for production

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import ScrollToTop from "./ScrollToTop";
 import { BrowserRouter } from 'react-router-dom';
